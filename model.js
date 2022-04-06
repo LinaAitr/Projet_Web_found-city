849f3d10deb786c7d214403838dfa49f04e1328b
a20005638 <lina.ait-razzouk@etu.univ-amu.fr>
2022-04-06 14:42:55 +0200

    structure a modifier debut modification

--- model.js
+++ model.js
@@ -5,71 +5,43 @@ const Sqlite = require('better-sqlite3');
 let db = new Sqlite('db.sqlite');
 
 exports.read = (id) => {
-  let found = db.prepare('SELECT * FROM recipe WHERE id = ?').get(id);
+  let found = db.prepare('SELECT * FROM monument WHERE id = ?').get(id);
   if(found !== undefined) {
-    found.ingredients = db.prepare('SELECT name FROM ingredient WHERE recipe = ? ORDER BY rank').all(id);
-    found.stages = db.prepare('SELECT description FROM stage WHERE recipe = ? ORDER BY rank').all(id);
+    found.region = db.prepare('SELECT name FROM region WHERE monument = ? ORDER BY rank').all(id);
+    //found.stages = db.prepare('SELECT description FROM stage WHERE recipe = ? ORDER BY rank').all(id);
     return found;
   } else {
     return null;
   }
 };
 
-exports.create = function(recipe) {
-  var id = db.prepare('INSERT INTO recipe (title, img, description, duration) VALUES (@title, @img, @description, @duration)').run(recipe).lastInsertRowid;
+exports.create = function(monument) {
+  var id = db.prepare('INSERT INTO monument (title, img, description, duration) VALUES (@title, @img, @description, @duration)').run(recipe).lastInsertRowid;
 
-  var insert1 = db.prepare('INSERT INTO ingredient VALUES (@recipe, @rank, @name)');
-  var insert2 = db.prepare('INSERT INTO stage VALUES (@recipe, @rank, @description)');
+  var insert1 = db.prepare('INSERT INTO regions VALUES (@recipe, @rank, @name)');
+  //var insert2 = db.prepare('INSERT INTO stage VALUES (@recipe, @rank, @description)');
 
-  var transaction = db.transaction((recipe) => {
-    for(let j = 0; j < recipe.ingredients.length; j++) {
-      insert1.run({recipe: id, rank: j, name: recipe.ingredients[j].name});
+  var transaction = db.transaction((monument) => {
+    for(let j = 0; j < monument.region.length; j++) {
+      insert1.run({monument: id, rank: j, name: monument.region[j].name});
     }
-    for(var j = 0; j < recipe.stages.length; j++) {
-      insert2.run({recipe: id, rank: j, description: recipe.stages[j].description});
-    }
+    // for(var j = 0; j < recipe.stages.length; j++) {
+    //   insert2.run({recipe: id, rank: j, description: recipe.stages[j].description});
+    // }
   });
 
-  transaction(recipe);
+  transaction(monument);
   return id;
 }
 
-exports.update = function(id, recipe) {
-  var result = db.prepare('UPDATE recipe SET title = @title, img = @img, description = @description WHERE id = ?').run(recipe, id);
-  if(result.changes == 1) {
-    var insert1 = db.prepare('INSERT INTO ingredient VALUES (@recipe, @rank, @name)');
-    var insert2 = db.prepare('INSERT INTO stage VALUES (@recipe, @rank, @description)');
-
-    var transaction = db.transaction((recipe) => {
-      db.prepare('DELETE FROM ingredient WHERE recipe = ?').run(id);
-      for(var j = 0; j < recipe.ingredients.length; j++) {
-        insert1.run({recipe: id, rank: j, name: recipe.ingredients[j].name});
-      }
-      db.prepare('DELETE FROM stage WHERE recipe = ?').run(id);
-      for(var j = 0; j < recipe.stages.length; j++) {
-        insert2.run({recipe: id, rank: j, description: recipe.stages[j].description});
-      }
-    });
-
-    transaction(recipe);
-    return true;
-  }
-  return false;
-}
-
-exports.delete = function(id) {
-  db.prepare('DELETE FROM recipe WHERE id = ?').run(id);
-  db.prepare('DELETE FROM ingredient WHERE recipe = ?').run(id);
-  db.prepare('DELETE FROM stage WHERE recipe = ?').run(id);
-}
 
 exports.search = (query, page) => {
   const num_per_page = 32;
   query = query || "";
   page = parseInt(page || 1);
 
-  var num_found = db.prepare('SELECT count(*) FROM recipe WHERE title LIKE ?').get('%' + query + '%')['count(*)'];
-  var results = db.prepare('SELECT id as entry, title, img FROM recipe WHERE title LIKE ? ORDER BY id LIMIT ? OFFSET ?').all('%' + query + '%', num_per_page, (page - 1) * num_per_page);
+  var num_found = db.prepare('SELECT count(*) FROM monument WHERE title LIKE ?').get('%' + query + '%')['count(*)'];
+  var results = db.prepare('SELECT id as entry, title, img FROM monument WHERE title LIKE ? ORDER BY id LIMIT ? OFFSET ?').all('%' + query + '%', num_per_page, (page - 1) * num_per_page);
 
   return {
     results: results,
